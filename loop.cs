89a4a90d448dc935809dfe6a8fe75dd0a9372bf5
CodeWithPartha <153088732+CodeWithPartha@users.noreply.github.com>
2023-12-07 04:07:24 -0800

    Partha

---
+++ loop.cs
@@ -0,0 +1,101 @@
+using System;
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        /*Console.Write("Enter the value:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        
+        while (a <= 5)
+        {
+            Console.WriteLine("C# For Loop: " + a);
+            a++;
+        }
+
+        Console.WriteLine("hello " + a);*/
+        
+   /*     int i=1;
+			while (i<=5)
+			{
+				Console.WriteLine("C# For Loop: Iteration {0}", i);
+				i++;
+			}*/
+			
+	//while loop to compute sum of first 5 natural numbers
+	
+/*	int i=1;
+	int sum=0;
+	while(i<=5) {
+	    sum+=i;
+	    i++;
+	}
+	Console.WriteLine("Sum of first 5 natural numbers is = {0}", sum);*/
+	
+	
+	//do...while loop
+/*		int i = 1, n = 5, product;
+
+			do
+			{
+				product = i * n;
+	Console.WriteLine("{0} * {1} = {2}", i, n, product);
+				i++;
+			} while (i <= 10);*/
+			
+/*			int x = 1, y = 3, sum;
+			do
+			{
+			    sum = x + y;
+			Console.WriteLine("{0} + {1} = {2}", x, y, sum);
+			x++;
+			}while (x <= 10);*/
+			
+/*			for (int i=1; i<=10; i++)
+			{
+				Console.WriteLine(i);
+			}*/
+			
+			/*for (int i=10; i>0; i--)
+			{
+				Console.WriteLine(i);
+			}
+			Console.WriteLine("Happy");*/
+			
+		/*	int n=5;
+         	int sum=0;
+         	for (int i=1; i <= n; i++)
+         	{
+         	    sum+=i;
+         	}
+Console.WriteLine("Sum of first {0} natural numbers = {1}", n, sum);*/
+
+
+    /*Console.Write("How many rows?");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    
+    Console.Write("How many columns?");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    
+     Console.Write("what is the symbol?");
+    String symbol= Console.ReadLine();  
+    
+    for (int i=0; i<rows; i++) {
+        for (int j=0; j<columns; j++)
+        {
+            Console.Write(symbol);
+        }
+        Console.WriteLine();
+    }*/
+    
+    // Nested for Loop to Print Pattern
+    
+/*    for (int i=1; i<=6; i++)
+			{
+				for (int j=1; j<=i; j++)
+				{
+					Console.Write(j + " ");
+				}
+				Console.WriteLine();
+			}*/
+    }
+}
